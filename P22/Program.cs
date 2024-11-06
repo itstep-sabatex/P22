02748a598bb16a711f8140b41c6cda2cf86d3450
sabatex <serhiy.lakas@hotmail.com>
2024-11-06 20:47:30 +0200

    Add project files.

---
+++ P22/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+
+// NET 2.0,3.0,4.0,4.8- несумісні (5.0,6.0,7.0,8.0)
+// Net Core 1.0,2.0,3.0,3.1,3.2 (5.0,6.0,7.0,8.0)
+
+using (IDbConnection dbConnection = new System.Data.SQLite.SQLiteConnection("FileName=C:/Users/serhi/.databases/cafe.db"))
+{
+
+}
