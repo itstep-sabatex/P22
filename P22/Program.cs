a8b5e6a5ac4cae20817a052f4a9c610eb5bca60a
sabatex <serhiy.lakas@hotmail.com>
2024-11-06 21:29:11 +0200

    *

--- P22/Program.cs
+++ P22/Program.cs
@@ -6,7 +6,9 @@ Console.WriteLine("Hello, World!");
 // NET 2.0,3.0,4.0,4.8- несумісні (5.0,6.0,7.0,8.0)
 // Net Core 1.0,2.0,3.0,3.1,3.2 (5.0,6.0,7.0,8.0)
 
-using (IDbConnection dbConnection = new System.Data.SQLite.SQLiteConnection("FileName=C:/Users/serhi/.databases/cafe.db"))
+using (IDbConnection dbConnection = new System.Data.SQLite.SQLiteConnection("DataSource=C:/Users/serhi/.databases/cafe.db"))
 {
+    dbConnection.Open();
+    dbConnection.Close();
 
 }
