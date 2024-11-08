c775d1c391d3a439f09d7e64bb738b8ca486ce29
sabatex <serhiy.lakas@hotmail.com>
2024-11-08 21:32:53 +0200

    lesson 2

---
+++ ADONETWPFDemo/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADONETWPFDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Waiter> waiters;
+        public MainWindow()
+        {
+            InitializeComponent();
+            waiters = new List<Waiter>();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (IDbConnection dbConnection = new System.Data.SQLite.SQLiteConnection("DataSource=C:/Users/serhi/.databases/itstep/cafe.db"))
+            {
+                dbConnection.Open();
+                waiters.Clear();
+
+                waiters.Add(new Waiter { Id = 1, Name = "Tester", Password = "******", Birthday = new DateTime(2000, 5, 1) });
+                dg.ItemsSource = waiters;
+
+                var dbCommand = dbConnection.CreateCommand();
+                dbCommand.CommandText = "select * from Waiters";
+                var dbReader = dbCommand.ExecuteReader();
+                while (dbReader.Read())
+                {
+                    Console.WriteLine($"{dbReader.GetInt32(0)} {dbReader.GetString(1)} {dbReader.GetString(2)} {dbReader.GetString(3)}");
+                }
+
+                dbConnection.Close();
+
+            }
+
+        }
+    }
+}
--- P22/Program.cs
+++ P22/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ADONETWPFDemo;
 using System.Data;
 
 Console.WriteLine("Hello, World!");
@@ -6,9 +7,74 @@ Console.WriteLine("Hello, World!");
 // NET 2.0,3.0,4.0,4.8- несумісні (5.0,6.0,7.0,8.0)
 // Net Core 1.0,2.0,3.0,3.1,3.2 (5.0,6.0,7.0,8.0)
 
+<<<<<<< HEAD
+var str = File.ReadAllText("E:/MOCK_DATA.json");
+var data = System.Text.Json.JsonSerializer.Deserialize<Waiter[]>(str);
+
+
+
+using (IDbConnection dbConnection = new System.Data.SQLite.SQLiteConnection("DataSource=C:/Users/serhi/.databases/itstep/cafe.db"))
+{
+    dbConnection.Open();
+
+    var tr = dbConnection.BeginTransaction();
+   // var dbCommand = dbConnection.CreateCommand();
+    //dbCommand.Transaction = tr;
+    //dbCommand.CommandText = "insert into Waiters (name,password,birthday) values (@name, @password,@birthday)";
+    //var parName = dbCommand.CreateParameter();
+    //parName.ParameterName = "name";
+    //var parPassword = dbCommand.CreateParameter();
+    //parPassword.ParameterName = "password";
+    //var parBirthday = dbCommand.CreateParameter();
+    //parBirthday.ParameterName = "birthday";
+    try
+    {
+        foreach (var item in data)
+        {
+            var dbCommand = dbConnection.CreateCommand();
+            dbCommand.Transaction = tr;
+
+            dbCommand.CommandText = "insert into Waiters (name,password,birthday) values (@name, @password,@birthday)";
+            var parName = dbCommand.CreateParameter();
+            parName.ParameterName = "name";
+            var parPassword = dbCommand.CreateParameter();
+            parPassword.ParameterName = "password";
+            var parBirthday = dbCommand.CreateParameter();
+            parBirthday.ParameterName = "birthday";
+
+            parName.Value = item.name;
+            parPassword.Value = item.password;
+            parBirthday.Value = item.birthday;
+            dbCommand.Parameters.Add(parName);
+            dbCommand.Parameters.Add(parPassword);
+            dbCommand.Parameters.Add(parBirthday);
+            dbCommand.ExecuteNonQuery();
+        }
+        tr.Commit();
+        Console.WriteLine("Commit");
+    }
+    catch (Exception ex)
+    {
+        tr.Rollback();
+        Console.WriteLine($"Error:{ex.Message} Rollback");
+    }
+
+   
+
+
+    var dbCommand1 = dbConnection.CreateCommand();
+    dbCommand1.CommandText = "select * from Waiters";
+    var dbReader = dbCommand1.ExecuteReader();
+    while (dbReader.Read())
+    {
+        Console.WriteLine($"{dbReader.GetInt32(0)} {dbReader.GetString(1)} {dbReader.GetString(2)} {dbReader.GetString(3)}");
+    }
+
+=======
 using (IDbConnection dbConnection = new System.Data.SQLite.SQLiteConnection("DataSource=C:/Users/serhi/.databases/cafe.db"))
 {
     dbConnection.Open();
+>>>>>>> a8b5e6a5ac4cae20817a052f4a9c610eb5bca60a
     dbConnection.Close();
 
 }
