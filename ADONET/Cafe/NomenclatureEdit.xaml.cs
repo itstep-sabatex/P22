df4f68861ce2e1426b15a962ef806005c38cca71
sabatex <serhiy.lakas@hotmail.com>
2024-11-27 21:28:54 +0200

    add homework

--- ADONET/Cafe/NomenclatureEdit.xaml.cs
+++ ADONET/Cafe/NomenclatureEdit.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cafe.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,10 +22,49 @@ namespace Cafe
     public partial class NomenclatureEdit : UserControl
     {
         public int? Id { get; set; }
-
+        
+        public event Action<bool> Success;
+        public Nomenclature?  EditItem { get; set; }
         public NomenclatureEdit()
         {
             InitializeComponent();
+            
+        }
+
+        public void Refresh()
+        {
+            if (Id != null)
+            {
+                using (var dbContext = Config.GetDbContext())
+                {
+                    EditItem = dbContext.Nomenclatures.Find(Id); // ??  
+                }
+            }
+            else
+            {
+                EditItem = new Nomenclature();
+            }
+        }
+
+        private void Button_ClickOk(object sender, RoutedEventArgs e)
+        {
+            using (var dbContext = Config.GetDbContext())
+            {
+
+                if (Id == null)
+                {
+                    dbContext.Add(EditItem);
+                }
+                else
+                {
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private void Button_ClickCancel(object sender, RoutedEventArgs e)
+        {
+            Success?.Invoke(false);
         }
     }
 }
