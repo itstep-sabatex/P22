7047a5c9c9847df76aa0a7b91aa94e042d48c47b
sabatex <serhiy.lakas@hotmail.com>
2024-12-06 20:58:42 +0200

    *

--- SystemProg/ProcessDemo/Program.cs
+++ SystemProg/ProcessDemo/Program.cs
@@ -4,29 +4,53 @@ using System.Diagnostics;
 Console.WriteLine("Hello, World!");
 
 var count = args.Count();
+void RunNotepad()
+{
+    var proc = new Process();
+    proc.StartInfo.FileName = "notepad.exe";
+    proc.Start();
 
-//var proc = new Process();
-//proc.StartInfo.FileName = "notepad.exe";
-//proc.Start();
+    Thread.Sleep(1000);
 
-//Thread.Sleep(1000);
+    proc.Kill();
+}
+void RunCalc()
+{
+    var proc = new Process();
+    proc.StartInfo.FileName = @"C:\Users\serhi\.repos\sabatex-itstep\P22\SystemProg\ProcessDemoCalc\bin\Debug\net9.0\ProcessDemoCalc.exe";
+    proc.StartInfo.Arguments = "10 20 30";
+    proc.StartInfo.WorkingDirectory = @"C:\Users\serhi\.repos\sabatex-itstep\P22\SystemProg\ProcessDemoCalc\bin\Debug\net9.0\";
+    proc.StartInfo.CreateNoWindow = true;
+    proc.StartInfo.RedirectStandardOutput = true;
+    proc.StartInfo.RedirectStandardError = true;
+    proc.OutputDataReceived += (a, b) => Console.WriteLine(b.Data);
+    proc.ErrorDataReceived += (a, b) => Console.WriteLine(b.Data);
+    proc.Start();
+    proc.BeginOutputReadLine();
+    proc.BeginErrorReadLine();
+    proc.WaitForExit();
+    Console.WriteLine(proc.ExitCode);
+    Thread.Sleep(3000);
+}
 
-//proc.Kill();
+bool ArchiveFolder(string folder,string resultFile)
+{
+    string script = $"tar -cvjf {resultFile} {folder}";
+    var proc = new Process();
+    proc.StartInfo.FileName = "cmd.exe";
+    proc.StartInfo.Arguments = $"/C {script}";
+    proc.StartInfo.UseShellExecute = false;
+    proc.StartInfo.RedirectStandardOutput = true;
+    proc.StartInfo.RedirectStandardError = true;
+    proc.OutputDataReceived += (a, b) => Console.WriteLine(b.Data);
+    proc.ErrorDataReceived += (a, b) => Console.WriteLine(b.Data);
+    proc.StartInfo.CreateNoWindow = true;
+    proc.Start();
+    proc.BeginOutputReadLine();
+    proc.BeginErrorReadLine();
+    proc.WaitForExit();
+    return proc.ExitCode == 0 || proc.ExitCode == 1000;
+}
 
 
-var proc = new Process();
-proc.StartInfo.FileName = @"C:\Users\serhi\.repos\sabatex-itstep\P22\SystemProg\ProcessDemoCalc\bin\Debug\net9.0\ProcessDemoCalc.exe";
-proc.StartInfo.Arguments = "10 20 30";
-proc.StartInfo.WorkingDirectory = @"C:\Users\serhi\.repos\sabatex-itstep\P22\SystemProg\ProcessDemoCalc\bin\Debug\net9.0\";
-proc.StartInfo.CreateNoWindow = true;
-proc.StartInfo.RedirectStandardOutput  = true;
-proc.StartInfo.RedirectStandardError = true;
-proc.OutputDataReceived += (a, b) => Console.WriteLine(b.Data);
-proc.ErrorDataReceived += (a, b) => Console.WriteLine(b.Data);
-
-proc.Start();
-proc.BeginOutputReadLine();
-proc.BeginErrorReadLine();
-proc.WaitForExit();
-Console.WriteLine(proc.ExitCode);
-Thread.Sleep(3000);
+ArchiveFolder(@"C:\temp\ProcessDemo", "processDemo.tar.bz2");
