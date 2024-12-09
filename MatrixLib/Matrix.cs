31446aa842af64c3904acb1f3d672191d93d5f33
sabatex <serhiy.lakas@hotmail.com>
2024-12-09 20:46:39 +0200

    add matrix

---
+++ MatrixLib/Matrix.cs
@@ -0,0 +1,33 @@
+﻿namespace MatrixLib
+{
+    public class Matrix
+    {
+        public double[,] CreateMatrix(int dimension)
+        {
+            var result = new double[dimension, dimension];
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    result[i, j] = Random.Shared.NextDouble();
+                }
+            }
+            return result;
+        }
+
+        public void MultiplreOneElement(object? param)
+        {
+            if (param == null)
+                throw new ArgumentNullException(nameof(param));
+            MatrixParams matrixParams = (MatrixParams)param;
+            double result = 0;
+            for (int mi = 0; mi < matrixParams.dim; mi++)
+            {
+                result = result + matrixParams.a[matrixParams.i, mi] * matrixParams.b[mi, matrixParams.j];
+            }
+            matrixParams.c[matrixParams.i, matrixParams.j] = result;
+        }
+
+
+    }
+}
---
+++ SystemProg/ThreadDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+void ThreadDemo()
+{
+    Console.WriteLine("Thread start");
+    Thread.Sleep(5000);
+}
+
+
+void ThreadDemo1(object a)
+{
+    Console.WriteLine($"Thread start - {a}");
+    Thread.Sleep(5000);
+}
+
+var thread = new Thread(ThreadDemo1);
+//thread.IsBackground = true;
+thread.Start("1");
+thread = new Thread(ThreadDemo1);
+//thread.IsBackground = true;
+thread.Start("2");
+thread = new Thread(ThreadDemo1);
+//thread.IsBackground = true;
+thread.Start("3");
+//thread.IsBackground = true;
+//thread.Suspend();
+for (int i = 0; i < 10; i++)
+{
+    var v = i;
+
+
+}
+
+//thread.Join();
+//thread.Resume();
+Console.WriteLine("Hello, World!");
+
+
