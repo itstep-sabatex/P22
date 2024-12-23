06763a418cf314cb208e10434f0080166a8caaa2
sabatex <serhiy.lakas@hotmail.com>
2024-12-23 18:51:18 +0200

    *

---
+++ DLLUseDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+[DllImport("user32.dll", CallingConvention = CallingConvention.StdCall)] //1.0,1.1,1.2
+static extern void MessageBox(IntPtr hWhd, string tesxt, string caption, uint uType = 0x02);
+IntPtr ptr = Process.GetCurrentProcess().MainWindowHandle;
+
+MessageBox(ptr, "Hello world", "Test");
+
+Console.WriteLine("Hello, World!");
---
+++ SemaphoreDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading;
+
+Console.WriteLine("Hello, World!");
+Semaphore semaphore = new Semaphore(2, 2, "9F633EE0-BD71-4F4C-A9DB-FFD865FC2D64", out bool isNew);
+
+try
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if (semaphore.WaitOne(TimeSpan.FromSeconds(10)))
+        {
+            Console.WriteLine($"Counter {i}");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            semaphore.Release();
+
+        }
+        else
+        {
+            Console.WriteLine("Error, not enter thread {i}");
+        }
+    }
+}
+finally
+{
+    semaphore.Close();
+}
