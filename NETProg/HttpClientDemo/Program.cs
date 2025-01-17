6e798dd08a67ee748930ddcf9e29a3c04f4487ee
sabatex <serhiy.lakas@hotmail.com>
2025-01-17 20:50:14 +0200

    *

---
+++ NETProg/HttpClientDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//var client = new HttpClient();
+//client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+//client.DefaultRequestHeaders.Accept.Clear();
+//client.DefaultRequestHeaders.Accept.Add(
+//    new MediaTypeWithQualityHeaderValue("application/json"));
+//client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+//await cllient.PostAsJsonAsync("posts", new { title = "foo", body = "bar", userId = 1 }); // POST https://jsonplaceholder.typicode.com/posts
+
+//var responce = await client.PostAsync("posts/1"); // GET https://jsonplaceholder.typicode.com/posts/1
+
+
+//var response = await client.GetAsync("posts"); // GET https://jsonplaceholder.typicode.com/posts
+
+using Sabatex.ObjectsExchange.Models;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Http.Json;
+
+var client = new HttpClient();
+client.BaseAddress = new Uri("https://sabatex.francecentral.cloudapp.azure.com");
+var login = new { clientId = new Guid("2a2c2a72-0ecb-4369-8e1e-c84be1f201ee"), password = "Aa1234567890-=" };
+
+var responce = await client.PostAsJsonAsync("api/v0/login", login); // POST https://sabatex.francecentral.cloudapp.axure.com/api/auth/login
+
+if (responce.IsSuccessStatusCode)
+{
+    var content = await responce.Content.ReadAsStringAsync();
+    Token token =System.Text.Json.JsonSerializer.Deserialize<Token>(content);
+    client.DefaultRequestHeaders.Add("clientId", "2a2c2a72-0ecb-4369-8e1e-c84be1f201ee");
+    client.DefaultRequestHeaders.Add("destinationId", "2a2c2a72-0ecb-4369-8e1e-c84be1f201ef");
+    client.DefaultRequestHeaders.Add("apiToken", token.AccessToken);
+
+    var objectValue = new {objectType= "Test", objectId = "1",text="Hello from client1" };
+    var responce2 = await client.PostAsJsonAsync("api/v0/objects", objectValue); // POST https://sabatex.francecentral.cloudapp.axure.com/api/object
+    if (responce2.IsSuccessStatusCode)
+    {
+        var content2 = await responce2.Content.ReadAsStringAsync();
+        Console.WriteLine(content2);
+    }
+    else
+    {
+        Console.WriteLine($"Failed to get data, status code: {responce2.StatusCode}");
+    }
+
+
+    Console.WriteLine(content);
+}
+else
+{
+    Console.WriteLine($"Failed to get data, status code: {responce.StatusCode}");
+}
+Console.ReadLine();
+
+
+
+//var responce = await client.GetAsync("/"); // GET https://www.google.com/
+//if (responce.IsSuccessStatusCode)
+//{
+//    var content = await responce.Content.ReadAsStringAsync();
+//    Console.WriteLine(content);
+//}
+//else
+//{
+//    Console.WriteLine($"Failed to get data, status code: {responce.StatusCode}");
+//}
+
+
+//WebClient client = new WebClient();  // NET Framework 3.5 (4.0) 
