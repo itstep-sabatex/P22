8e8d74354ab72bffc947db12a31415f7e2c6b11c
sabatex <serhiy.lakas@hotmail.com>
2025-02-05 19:43:06 +0200

    *

--- Cafe.Models/Role.cs
+++ Cafe.Models/Role.cs
@@ -16,7 +16,7 @@ namespace Cafe.Models
         public IEnumerable<UserRole> UserRoles { get; set; }
         // test
         public static Role Admin => new Role{Id=1,Name="Admin"};
-        public static Role Manager => new Role { Id = 2, Name = "Manager" };
+        public static Role Manager => new Role { Id = 2, Name = "Manager1" };
         public static Role Waiter => new Role { Id = 3, Name = "Waiter" };
 
 
